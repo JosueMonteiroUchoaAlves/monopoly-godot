9a0f7d3a538e6f3fe7df76a98bfa8c0a4b240e47
Josue Monteiro <jmua@aluno.ifnmg.edu.br>
2026-07-09 21:59:12 -0300

    feat(Board): implementing the board class fix(file names): removing "abstract" from the file names

--- scripts/board.gd
+++ scripts/board.gd
@@ -2,4 +2,20 @@ class_name Board extends Node
 
 const SIZE = 9
 
-# TODO: vou colocar o resto da classe Board aqui tambem
+@export var _tiles: Array[Tile]
+@export var _players: Array[Controller]
+
+func _init(tiles: Array[Tile], players: Array[Controller]):
+	_tiles = tiles
+	_players = players
+
+func display_board():
+	pass
+
+func execute_turn():
+	for controller: Controller in _players:
+		var position: int = controller.player.get_position()
+		controller.play.activate_controller()
+		var new_position: int = controller.player.get_position()
+		if position != new_position:
+			_tiles[new_position].activate(controller.player)		
